115579aba15d61bd21b6ee137c16282d3e55390e
Dalton Hart <dalton-hart@uiowa.edu>
2015-07-10 20:51:27 -0500

    update README

--- script.js
+++ script.js
@@ -1,30 +1,63 @@
 $(document).ready(function(){
-	var row = 16;
 	
+	var reset = function(row){
 		for (i = 0; i<row; i++){
 			for (j = 0; j<row; j++){
 				$('#wrapper').append("<div class = 'block'></div>");
 			};
 		};
-	$('.block').on('mouseenter',function(){
-		$(this).css('background-color','#'+ Math.floor(Math.random()*16777215).toString(16));
-	});
-
-	$('button').on('click', function(){
-		$('#wrapper').empty();
-
-		row = prompt("How many rows", row);
-		for (i = 0; i<row; i++){
-			for (j = 0; j<row; j++){
-				$('#wrapper').append("<div class = 'block'></div>");
-			};
-		};
-		$('.block').css({'height':960/row + 'px','width':960/row + 'px'});
+	};
+	var rand = function(){
 		$('.block').on('mouseenter',function(){
 			$(this).css('background-color','#'+ Math.floor(Math.random()*16777215).toString(16));
 		});
+	};
+	var trail = function(op){
+		$('.block').fadeTo('fast',.01);
+		$('.block').on('mouseenter',function(){
+			$(this).css('opacity','1');
+			$(this).fadeTo('slow',op);
+		});
+	};
+	var solid = function(color){
+		
+		$('.block').on('mouseenter',function(){
+			$(this).css('background-color',color);
+		});
+	};
+
+	var color = 'blue';
+	var row = 16;
+	reset(row);
+	solid(color);
+
+	$('#reset').on('click', function(){
+		$('#wrapper').empty();
+		row = prompt("How many rows", row);
+		reset(row);
+		$('.block').css({'height':960/row + 'px','width':960/row  + 'px'});
+		solid(color);
+		document.getElementById("trail").checked = false;
 	});
-	//$('.block').on('mouseenter',function(){
-		//$(this).css('background-color','blue');//'#'+ Math.floor(Math.random()*16777215).toString(16));
-	//});
+
+	$('#trail').on('click',function(){
+		var trailBox = document.getElementById("trail").checked;
+		if(trailBox === true){
+			var op = .01;
+			trail(op);
+		} else {
+			$('#wrapper').empty();
+			reset(row);	
+			solid(color);					
+		};
+	});
+
+	$('#rainbow').on('click',function(){
+		rand();
+	});
+
+	$('#solid').on('click',function(){
+		color = prompt("what color");
+		solid(color);
+	});	
 });
